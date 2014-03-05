a5e2df7e0bb90fe95a00704f310d325e4d90ad84
Cristian Harja <cristian.harja@gmail.com>
2014-03-06 00:42:48 +0200

    Implemented the `StNode` class which should be used to build syntax trees for the LDF parser.

---
+++ ldf-core/src/main/java/ldf/parser/TokenFactory.java
@@ -0,0 +1,49 @@
+package ldf.parser;
+
+import ldf.java_cup.runtime.Symbol;
+import ldf.java_cup.runtime.TokenFactoryImpl;
+
+/**
+ * @author Cristian Harja
+ */
+public final class TokenFactory extends TokenFactoryImpl {
+
+    private StNode lastToken;
+
+    @Override
+    protected Symbol newSymbol(
+            String symName, int symCode, int parse_state
+    ) {
+        return new StNode(symName, symCode, parse_state);
+    }
+
+    @Override
+    public Symbol newToken(
+            String symName, int symCode,
+            int lineL, int columnL, int offsetL
+    ) {
+        StNode tok = (StNode) super.newToken(
+                symName, symCode,
+                lineL, columnL, offsetL
+        );
+        if (lastToken != null) {
+            StNode.linkTokens(lastToken, tok);
+        }
+        lastToken = tok;
+
+        return tok;
+    }
+
+    @Override
+    public Symbol newEOF(
+            String symName, int symCode,
+            int lineL, int columnL, int offsetL
+    ) {
+        Symbol eof = super.newEOF(
+                symName, symCode,
+                lineL, columnL, offsetL
+        );
+        lastToken = null;
+        return eof;
+    }
+}
